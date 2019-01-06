af73a344af9bbeb8b2ce32104a57c4c57f7bf361
Janette <tokita_janette@hotmail.com>
2019-01-05 18:22:44 -0600

    001.000.002 - Creación de tabla Clientes
    Creación de migración para tabla Clientes.

--- migrations/m190105_211722_tabla_Cliente.php
+++ migrations/m190105_211722_tabla_Cliente.php
@@ -12,7 +12,29 @@ class m190105_211722_tabla_Cliente extends Migration
      */
     public function safeUp()
     {
+        $this->createTable('Cliente', [
+           'id' =>  $this->primaryKey(),
+           'razon_social' => $this->string(250)->notNull(),
+           'rfc' => $this->string(250),
+           'id_giro' => $this->integer()->notNull(),
+           'id_pais' => $this->integer()->notNull(),
+           'id_estado' => $this->integer()->notNull(),
+           'id_municipio' => $this->integer()->notNull(),
+           'calle_colonia' => $this->string(250)->notNull(),
+           'codigo_postal' => $this->string(7)->notNull(),
+           'celular' => $this->string(12)->notNull(),
+           'telefono' => $this->string(12)->notNull(),
+           'correo' => $this->string(250)->notNull(),
+           'cedula_profesional' => $this->string(250),
+           'estatus' => $this->tinyInteger(1)->defaultValue(1)->notNull(),
+           'bloqueado' => $this->tinyInteger(1)->defaultValue(0)->notNull()
+       ]);
 
+       #Nombre de índice, tabla a afectar, columna a relacionar
+       $this->createIndex('idx-id_giro', 'Cliente', 'id_giro');
+       $this->createIndex('idx-estatus', 'Cliente', 'estatus');
+       $this->addCommentOnColumn ( 'Cliente', 'estatus', '1= Activo, 0= Eliminado ' );
+       $this->addCommentOnColumn ( 'Cliente', 'bloqueado', '1= Bloqueado por pago, 0= Desbloqueado' );
     }
 
     /**
